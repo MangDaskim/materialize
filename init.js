85af47eae746ed7724dd4d90ad8028c7bf49ae6b
MangDaskim <mangdaskim123@gmail.com>
2017-10-22 21:45:33 +0700

    Update init.js

--- init.js
+++ init.js
@@ -10,5 +10,14 @@
       onClose: function(el) { /* Do Stuff* / }, // A function to be called when sideNav is closed
     });
 
+  $('.pushpin-nav').each(function() {
+    var $this = $(this);
+    var $target = $('#' + $(this).attr('data-target'));
+    $this.pushpin({
+      top: $target.offset().top,
+      bottom: $target.offset().top + $target.outerHeight() - $this.height()
+    });
+  });
+  
   }); // end of document ready
 })(jQuery); // end of jQuery name space
